eb891430fd38a598b0f0e5b5883c6dff746294aa
David Hock <david.hock@henrichsen.de>
2021-10-25 19:42:55 +0200

    extension

---
+++ src/Mimp.SeeSharper.ObjectDescription.Extensions.Configuration/ObjectDescriptionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Mimp.SeeSharper.ObjectDescription.Abstraction;
+using System;
+
+namespace Mimp.SeeSharper.ObjectDescription.Extensions.Configuration
+{
+    public static class ConfigurationExtensions
+    {
+
+
+        public static IObjectDescription ToDescription(this IConfiguration configuration)
+        {
+            if (configuration is null)
+                throw new ArgumentNullException(nameof(configuration));
+
+            return new ConfigurationObjectDescription(configuration);
+        }
+
+
+    }
+}
